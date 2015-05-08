8f42d4cef9f7077b984b38e3930bcb88eff87fd6
Jesse <jesserbrightman@gmail.com>
2015-05-07 20:19:03 -0700

    added project

---
+++ Assignment1_WAS/Assignment1_WAS/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1_WAS.Models;
+using PagedList;
+
+namespace Assignment1_WAS.Controllers
+{
+    public class HomeController : Controller
+    {
+        ticketsEntities db = new ticketsEntities();
+            
+        // GET: Home
+        public ActionResult Index()
+        {
+            var events = db.Events.FirstOrDefault();
+            return View(events);
+        }
+
+        public ActionResult PayPal()
+        {
+            PayPal_IPN paypalResponse = new PayPal_IPN("test");
+
+            if (paypalResponse.TXN_ID != null)
+            {
+                IPN ipn = new IPN();
+                ipn.transactionID = paypalResponse.TXN_ID;
+                ipn.firstName = paypalResponse.PayerFirstName;
+                ipn.lastName = paypalResponse.PayerLastName;
+                ipn.buyerEmail = paypalResponse.PayerEmail;
+                ipn.txTime = DateTime.Now;
+                ipn.custom = System.Web.HttpContext.Current.Session.SessionID;
+                ipn.ticketQty = paypalResponse.Quantity;
+                decimal amount = Convert.ToDecimal(paypalResponse.PaymentGross);
+                ipn.amount = amount;            
+                ipn.paymentStatus = paypalResponse.PaymentStatus;
+                db.IPNs.Add(ipn);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ThankYou");
+        }
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAttendees(int? page)
+        {
+            var attendees = db.IPNs;
+            return View(attendees);           
+        }
+    }
+}
---
+++ Assignment1_WAS/Assignment1_WAS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment1_WAS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment1_WAS")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dae0703e-c7b2-41cf-9661-64829e45dc07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
